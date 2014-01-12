b36792b154e71aea12681eee60114310e10b12ca
Jordan Escoto <jordanescoto@gmail.com>
2014-01-11 17:18:22 -0800

    setup

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$("#mystory").hide();
+
+
+$("#comment").click(function() {
+	$(".user-comment-list").toggle();
+});
+
+$("#hide-comment").click(function() {
+	$(".user-comment-list").toggle();
+});
+
+$("#contact").click(function() {
+	$("#contact-modal").modal();
+});
+
+$("#my-connections").click(function() {
+	$("#my-connections").modal();
+});
+
+$("#mystory-show").click(function() {
+	$("#mystory").toggle();
+	$("#myfeed").toggle();
+});
+
+$("#mystory-hide").click(function() {
+	$("#mystory").toggle();
+});
